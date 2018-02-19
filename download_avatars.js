e477738e5d5984f78822477ca31e6c2a37517487
Greg Davis <gdavis111@live.ca>
2018-02-19 22:17:13 +0000

    created getRepoContributors function and added 'user-agent: request' line as well as request function

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,35 @@
 var request = require('request');
+var fs = require('fs');
+
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request'
+    }
+  };
+
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
+}
+
+// function getRepoContributors(repoOwner, repoName, cb) {
+//   request.get('https://api.github.com/repos/jquery/jquery/contributors')
+//         .on('error', function (err) {
+//           throw err;
+//         })
+//         .on('response', function(response) {
+//           console.log('Response Status Code: ', response.statusCode);
+//           console.log('Response content-type: ', response.headers['content-type']);
+//         })
+//         .pipe(fs.createWriteStream('./avatar.jpg'));
+// }
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
